3f0aa2705171539b493e11f1504d41077814b646
johnsiefert <johnsief12@gmail.com>
2021-10-30 08:06:04 -0400

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "braces", "parentheses", "brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What tag defines the body of the HTML document, and usually includes all the contents such as the text, hyperlinks, images, tables, lists, and more?",
+        choices: ["<head></head>", "<body></body>", "<title></title>", "<br>"],
+        answer: "<body></body>"
+    },
+    {
+        title: "What tag is used to define a hyperlink, or link to another page?",
+        choices: ["<strong>", "<blockquote>", "<em>", "<a>"],
+        answer: "<a>"
+    },
+    {
+        title: "CSS stands for ____ Style Sheets.",
+        choices: ["Curious", "Concept", "Cascading", "Concave"],
+        answer: "Cascading"
+    }
+];
